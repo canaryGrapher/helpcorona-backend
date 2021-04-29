eb951287e2382b9cee4fcc6414a6b33e31258f62
Yash Aryan <yash.aryan1@learner.manipal.edu>
2021-04-30 01:56:54 +0530

    Update apikeys.js

--- services/apikeys.js
+++ services/apikeys.js
@@ -7,18 +7,46 @@ const api_keys = [
         "email": "yasharyan307@outlook.com"
     },
     {
-        "keyID": "2_alpha_4325adsaf76",
+        "keyID": "2_aplha_d9eb3a42f46d459d9a128bdb2fac908e",
         "firstname": "Manorath",
         "lastname": "Khanna",
-        "phone": "+919663516444",
-        "email": "mkhanna@outlook.com"
+        "phone": "+916354839243",
+        "email": "mkhanna1509@gmail.com"
     },
     {
-        "keyID": "3_alpha_4325adsaf76",
-        "firstname": "Manorath",
-        "lastname": "Khanna",
-        "phone": "+919663516444",
-        "email": "mkhanna@outlook.com"
+        "keyID": "3_alpha_64fcdc86f9864775aff586870b13e1b6",
+        "firstname": "Dhruv",
+        "lastname": "Verma",
+        "phone": "+917006382694",
+        "email": "dhruv.muj16@gmail.com"
+    },
+    {
+        "keyID": "4_alpha_cced0a44d1224d5d940ec9c0cc6590d4",
+        "firstname": "Sharan",
+        "lastname": "Nagarajan",
+        "phone": "+918778949938",
+        "email": "nsharan2000@gmail.com"
+    },
+    {
+        "keyID": "5_alpha_04c1c554dd1740babdc630479245f7f4",
+        "firstname": "Suneel",
+        "lastname": "Raj",
+        "phone": "+919381484363",
+        "email": "govindsraj17@gmail.com"
+    },
+    {
+        "keyID": "6_alpha_9be327bf4d4c455aa1b2cf4d295ba5a8",
+        "firstname": "Mahek",
+        "lastname": "Jain",
+        "phone": "+919074871272",
+        "email": "mahek582jain@gmail.com"
+    },
+    {
+        "keyID": "7_alpha_bd52c319e76040fd8b6816148d22f6df",
+        "firstname": "Meet",
+        "lastname": "Shah",
+        "phone": "+919328243515",
+        "email": "meetsuchitparinashah@gmail.com"
     }
 ]
 
